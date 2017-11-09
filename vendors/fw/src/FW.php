8d427bc455d5ecfb37559164fc0372e1da80d2b2
dfrancklin23 <dfrancklin23@gmail.com>
2017-11-09 14:35:48 -0500

    [update]

--- vendors/fw/src/FW.php
+++ vendors/fw/src/FW.php
@@ -61,6 +61,9 @@ class FW {
 	}
 
 	public function run() {
+		// check for context folder
+		// create .htaccess if not exists
+		
 		$systemFolders = $this->config->get('system-folders');
 
 		$this->scanComponents(...$systemFolders);
